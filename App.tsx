d8c3dd230f9b830fe37259bb007f9d2c45b273f9
t-kusakabe <ttmac0704@gmail.com>
2020-10-13 21:58:57 +0900

    Use count component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { RecoilRoot } from 'recoil';
+
+import Count from './components/Count';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RecoilRoot>
+      <View style={styles.container}>
+        <Text>Sample Recoil</Text>
+        <Count />
+        <StatusBar style="auto" />
+      </View>
+    </RecoilRoot>
   );
 }
 
